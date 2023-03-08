7c2005d007ce5f81d7f782b0e3ce2e75e6008f34
Segey Smachek <sergeysmachek@gmail.com>
2023-03-08 20:54:15 +0300

    Add transactional to BookRepository

--- src/main/java/com/polarbookshop/catalogservice/domain/BookRepository.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/BookRepository.java
@@ -3,6 +3,7 @@ package com.polarbookshop.catalogservice.domain;
 import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -10,6 +11,7 @@ public interface BookRepository  extends CrudRepository<Book, Long> {
     Optional<Book> findByIsbn(String isbn);
     boolean existsByIsbn(String isbn);
     @Modifying
-    @Query("delete from Book where isbn = :isbn")
+    @Transactional
+    @Query("delete from book where isbn = :isbn")
     void deleteByIsbn(String isbn);
 }
