39f0a182331d97f51d0cd4a9ed15762326e34062
Segey Smachek <sergeysmachek@gmail.com>
2023-03-08 18:20:37 +0300

    Add JDBC auditing.

--- src/test/java/com/polarbookshop/catalogservice/web/BookJsonTests.java
+++ src/test/java/com/polarbookshop/catalogservice/web/BookJsonTests.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
 
 import java.io.IOException;
+import java.time.Instant;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
@@ -18,7 +19,7 @@ public class BookJsonTests {
 
     @Test
     void testSerialize() throws IOException {
-        var book = new Book((long) 1, "1234567890", "Title", "Author", 9.90, 0);
+        var book = new Book((long) 1, "1234567890", "Title", "Author", 9.90,  Instant.now(), Instant.now(), 0);
         var jsonContent = json.write(book);
 
         assertThat(jsonContent).extractingJsonPathNumberValue("@.id"). isEqualTo(book.id().intValue());
@@ -26,11 +27,14 @@ public class BookJsonTests {
         assertThat(jsonContent).extractingJsonPathStringValue("@.title").isEqualTo(book.title());
         assertThat(jsonContent).extractingJsonPathStringValue("@.author").isEqualTo(book.author());
         assertThat(jsonContent).extractingJsonPathNumberValue("@.price").isEqualTo(book.price());
+        assertThat(jsonContent).extractingJsonPathStringValue("@.createdDate").isEqualTo(book.createdDate().toString());
+        assertThat(jsonContent).extractingJsonPathStringValue("@.lastModifiedDate").isEqualTo(book.lastModifiedDate().toString());
         assertThat(jsonContent).extractingJsonPathNumberValue("@.version").isEqualTo(book.version());
     }
 
     @Test
     void testDeserialize() throws Exception {
+        var instant = Instant.parse("2022-03-08T17:30:40.135799Z");
         var content = """
                 {
                     "id":1,
@@ -38,11 +42,13 @@ public class BookJsonTests {
                     "title":"Title",
                     "author":"Author",
                     "price":9.90,
+                    "createdDate":"2022-03-08T17:30:40.135799Z",
+                    "lastModifiedDate":"2022-03-08T17:30:40.135799Z",
                     "version":0
                 }
                 """;
         assertThat(json.parse(content))
                 .usingRecursiveComparison()
-                .isEqualTo(new Book(1L, "1234567890", "Title", "Author", 9.90, 0));
+                .isEqualTo(new Book(1L, "1234567890", "Title", "Author", 9.90, instant, instant, 0));
     }
 }
